ccfbae79dce3193d4c62a538d2b1150108e51074
Justin Golden <justingolden21@gmail.com>
2018-08-31 02:54:59 -0700

    code polishing

--- scripts/char-scripts.js
+++ scripts/char-scripts.js
@@ -14,6 +14,8 @@ numDropped = 1;
 diceSides = 6;
 show method character was created in download file
 
+add history
+add percentile
 */
 
 let characters = [];
@@ -24,7 +26,7 @@ function Character() {
 	this.stats = [];
 	this.statTotal = 0;
 	this.modTotal = 0;
-	for(let i = 0; i < 6; i++) { //6 stats
+	for(let i=0; i<6; i++) { //6 stats
 		let newStat = new Stat();
 		this.stats.push(newStat);
 		this.statTotal += newStat.value;
@@ -34,11 +36,11 @@ function Character() {
 
 function Stat() { //rolls is array of 4 dice results
 	this.rolls = [];
-	for(let i = 0; i < 4; i++) { //4 rolls take highest 3
+	for(let i=0; i<4; i++) { //4 rolls take highest 3
 		this.rolls.push(getRoll(6) );
 	}
 	this.value = 0;
-	for(let i = 0, minIdx = this.rolls.indexOf(Math.min.apply(Math, this.rolls) ); i < 4; i++) {
+	for(let i=0, minIdx=this.rolls.indexOf(Math.min.apply(Math, this.rolls) ); i<4; i++) {
 		if(i != minIdx) {
 			this.value += this.rolls[i];
 		}
@@ -47,7 +49,7 @@ function Stat() { //rolls is array of 4 dice results
 }
 
 function getStatMod(stat) {
-	for(let i = 3, j = -4; ; i+=2, j++) {
+	for(let i=3, j=-4; ; i+=2, j++) {
 		if(i >= stat) {
 			return j;
 		}
@@ -55,88 +57,87 @@ function getStatMod(stat) {
 }
 
 function getDieCode(roll) {
-	return "&#98" + (55 + roll);
+	return '&#98' + (55 + roll);
 }
 
 function getDiceCodes(rolls) {
-	let diceCodes = "";
-	for(let i = 0; i < rolls.length; i++) {
-		diceCodes += getDieCode(rolls[i]) + " ";
+	let diceCodes = '';
+	for(let i=0; i<rolls.length; i++) {
+		diceCodes += getDieCode(rolls[i]) + ' ';
 	}
 	return diceCodes;
 }
 
 window.onload = function() {
 		
-	document.getElementById("download").onclick = function() {
-		console.log('downloading...');
+	$('#download').on('click',function() {
 		if(characters.length == 0) {
-			return;
+			return -1;
 		}
 		data = [];
 		let currentChar = characters[characters.length-1];
-		data.push(document.getElementById("nameInput").value + "\r\n");
-		for(let i = 0; i < currentChar.stats.length; i++) {
-			data.push(document.getElementById("stat" + (i+1) + "Mod").value + ": ");
-			for(let j = 0; j < currentChar.stats[i].rolls.length; j++) {
-				data.push(currentChar.stats[i].rolls[j].toString());
+		data.push($('#nameInput').val() + '\r\n');
+		for(let i=0; i<currentChar.stats.length; i++) {
+			data.push($('#stat' + (i+1) + 'Mod').val() + ': ');
+			for(let j=0; j<currentChar.stats[i].rolls.length; j++) {
+				data.push(currentChar.stats[i].rolls[j].toString() );
 				if(j < currentChar.stats[i].rolls.length-1) {
-					data.push(",");
+					data.push(',');
 				}
 			}
-			data.push("\tVal: " + currentChar.stats[i].value + "\tMod: " + currentChar.stats[i].mod );
-			data.push("\r\n");
+			data.push('\tVal: ' + currentChar.stats[i].value + '\tMod: ' + currentChar.stats[i].mod);
+			data.push('\r\n');
 		}
-		data.push("\r\nTotal:\t\tVal: " + currentChar.statTotal + "\tMod: " + currentChar.modTotal);
+		data.push('\r\nTotal:\t\tVal: ' + currentChar.statTotal + '\tMod: ' + currentChar.modTotal);
 		
 		properties = {type: 'plain/text'};
 		try {
-			file = new File(data, "stats.txt", properties);
+			file = new File(data, 'stats.txt', properties);
 		} catch (e) {
 			file = new Blob(data, properties);
 		}
 
-		document.getElementById("downloadLink").download = "Character-" + document.getElementById("nameInput").value + ".txt";
-		document.getElementById("downloadLink").href = URL.createObjectURL(file);
-		document.getElementById("downloadLink").click();
-	}
+		$('#downloadLink').prop('download', 'Character - ' + $('#nameInput').val() + '.txt');
+		$('#downloadLink').prop('href', URL.createObjectURL(file) );
+		document.getElementById('downloadLink').click(); //idk why this doesn't work with jquery selector
+	});
 	
-	document.getElementById("genButton").onclick = function() {
+	$('#genButton').on('click', function() {
 		resetStats();
-		document.getElementById("nameInput").value = "";
+		$('#nameInput').val('')
 		characters.push(new Character() );
 		let currentChar = characters[characters.length-1];
-		for(let i = 0; i < 6; i++) {
-			document.getElementById("stat" + (i+1) ).innerHTML = getDiceCodes(currentChar.stats[i].rolls) + "<br><b>" + currentChar.stats[i].value + "</b><br>" + (currentChar.stats[i].mod > 0 ? "+" : "") + currentChar.stats[i].mod;
+		for(let i=0; i<6; i++) {
+			$('#stat' + (i+1) ).html(getDiceCodes(currentChar.stats[i].rolls) + '<br><b>' + currentChar.stats[i].value + '</b><br>' + (currentChar.stats[i].mod > 0 ? '+' : '') + currentChar.stats[i].mod);
 		}
-		document.getElementById("statsInfoStdDev").innerHTML = "Deviation:<br><b>" + Math.round(stdDev(statsToValues(currentChar.stats))*100)/100 + "</b><br>" + Math.round(stdDev(statsToMods(currentChar.stats))*100)/100;
-		document.getElementById("statsInfoAvg").innerHTML = "Mean:<br><b>" + Math.round(currentChar.statTotal*100/6)/100 + "</b><br>" + Math.round(currentChar.modTotal*100/6)/100;
-		document.getElementById("statsInfo").innerHTML = "Total:<br><b>" + currentChar.statTotal+ "</b><br>" + currentChar.modTotal;
-		document.getElementById("numChars").innerHTML = characters.length + " character" + (characters.length > 1 ? "s" : "") + " generated";
+		$('#statsInfoStdDev').html('Deviation:<br><b>' + Math.round(stdDev(statsToValues(currentChar.stats))*100)/100 + '</b><br>' + Math.round(stdDev(statsToMods(currentChar.stats))*100)/100);
+		$('#statsInfoAvg').html('Mean:<br><b>' + Math.round(currentChar.statTotal*100/6)/100 + '</b><br>' + Math.round(currentChar.modTotal*100/6)/100);
+		$('#statsInfo').html('Total:<br><b>' + currentChar.statTotal+ '</b><br>' + currentChar.modTotal);
+		$('#numChars').html(characters.length + ' character' + (characters.length > 1 ? 's' : '') + ' generated');
 		
 		let numbers = [];
-		for(let i = 0; i < 6; i++) {
-			numbers.push(parseInt(currentChar.stats[i].value));
+		for(let i=0; i<6; i++) {
+			numbers.push(parseInt(currentChar.stats[i].value) );
 		}
 		numbers.sort(sortNumber);
-		document.getElementById("numbersInput").value = numbers.join(", ");
+		$('#numbersInput').val(numbers.join(', ') );
 		// makeChart();
-	}
+	});
 	
 	function sortNumber(a,b) {
 		return a - b;
 	}
 	
-	document.getElementById("genButton").click();
+	$('#genButton').click();
 	
-	let statMods = document.getElementsByClassName("statMod");
-	for(let i = 0; i < statMods.length; i++) {
+	let statMods = $('.statMod');
+	for(let i=0; i<statMods.length; i++) {
 		statMods[i].onclick = statMods[i].onfocus = function(evt) {
-			for(let j = 0; j < 6; j++) {
+			for(let j=0; j<6; j++) {
 				evt.target.options[j+1].disabled = false;			
 			}
-			for(let j = 0; j < 6; j++) {
-				let idx = document.getElementById("stat" + (j+1) + "Mod").selectedIndex;
+			for(let j=0; j<6; j++) {
+				let idx = $('#stat' + (j+1) + 'Mod').prop('selectedIndex');
 				if(idx != 0) {
 					evt.target.options[idx].disabled = true;
 				}
@@ -144,32 +145,28 @@ window.onload = function() {
 		}
 	}
 	
-	document.getElementById("copyButton").onclick = function() {
-		let numInput = document.getElementById("numbersInput");
-		numInput.focus();
-		numInput.setSelectionRange(0, numInput.value.length);
-		document.execCommand("copy");
-	}
+	$('#copyButton').on('click', function() {
+		let numInput = $('#numbersInput');
+		numInput.select();
+		document.execCommand('copy');
+	});
 	
-	document.getElementById("resetStat").onclick = function() {
+	$('#resetStat').on('click', function() {
 		resetStats();
-	}
+	});
 	
-	document.getElementById("nightMode").onclick = function() {
+	$('#nightMode').on('click', function() {
 		night = !night;
-		
-		let nightTheme = document.getElementById("nightTheme");
-		nightTheme.href = night ? "styles/night.css" : "";
-		// makeChart();
+		$('#nightTheme').prop('href', night ? 'styles/night.css' : '');
 		$('#titleImg').prop('src', night ? 'img/d20-white.svg' : 'img/d20.svg');
 		$('select').css('color', night ? '#fff' : '#333');
 		$('input').css('color', night ? '#fff' : '#333');
-		// document.getElementById("titleImg").src = night ? "dice/d20-light.svg" : "dice/d20-dark.svg";
-	}
+		// makeChart();
+	});
 	
-	document.getElementById("fullscreen").onclick = function() {
+	$('#fullscreen').on('click', function() {
 		toggleFullscreen();
-	}
+	});
 	
 }
 
@@ -179,7 +176,7 @@ window.onload = function() {
 
 function statsToValues(stats) {
 	let vals = [];
-	for(let i = 0; i < stats.length; i++) {
+	for(let i=0; i<stats.length; i++) {
 		vals[i] = stats[i].value;
 	}
 	return vals;
@@ -187,7 +184,7 @@ function statsToValues(stats) {
 
 function statsToMods(stats) {
 	let mods = [];
-	for(let i = 0; i < stats.length; i++) {
+	for(let i=0; i<stats.length; i++) {
 		mods[i] = stats[i].mod;
 	}
 	return mods;
@@ -195,26 +192,25 @@ function statsToMods(stats) {
 
 function stdDev(array) { //standard deviation
 	let sum = 0, len = array.length;
-	for(let i = 0; i < len; i++) {
+	for(let i=0; i<len; i++) {
 		sum += array[i];
 	}
 	let mean = sum / len;
 	let devs = 0;
-	for(let i = 0; i < len; i++) {
-		devs += Math.pow(array[i] - mean, 2);
+	for(let i=0; i<len; i++) {
+		devs += Math.pow(array[i]-mean, 2);
 	}
-	return Math.sqrt(devs / (len - 1) );	
+	return Math.sqrt(devs / (len-1) );	
 }
 
 function resetStats() {
-	let statMods = document.getElementsByClassName("statMod");
-	for(let i = 0; i < statMods.length; i++) {
+	let statMods = $('.statMod');
+	for(let i=0; i<statMods.length; i++) {
 		statMods[i].selectedIndex = 0;
 	}
 }
 
-
-
+//todo: use this
 function percentile(num) {
 	num -= 3;
 	let val=0;
