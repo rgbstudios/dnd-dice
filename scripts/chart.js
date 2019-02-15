5921527c30c92b2917a4974b626ca03e30f88b17
Justin Golden <justingolden21@gmail.com>
2019-02-15 03:48:29 -0800

    fix indentation

--- scripts/chart.js
+++ scripts/chart.js
@@ -1,97 +1,72 @@
 //todo: bug: sometime bars dont have width
 function makeChart() {
-  google.charts.load('current', {'packages':['corechart']});
-  google.charts.setOnLoadCallback(drawChart);
+	google.charts.load('current', {'packages':['corechart']});
+	google.charts.setOnLoadCallback(drawChart);
 }
 
 function drawChart() {
 
-  let graphColor = isNight ? '#ccc' : '#333';
+	let graphColor = isNight ? '#ccc' : '#333';
 
-  //bar
-  let chartdata = [['','', { role: 'style' } ]];
-  for(let i=0; i<odds.length; i++) {
-    chartdata.push([i+3, odds[i], '#369']);
-  }
+	//begin chart 1: odds of each roll
+	let chartdata = [['','', { role: 'style' } ]];
+	for(let i=0; i<odds.length; i++)
+		chartdata.push([i+3, odds[i], '#369']);
 
- //  for(let i=0; i<6; i++) {
- //    //hardcoded 20 to display larger as if 20 occurences of stat
- //    //todo: doesnt work for repeats of same stat
- //    chartdata.push([characters[characters.length-1].stats[i].value, 20, '#fff']);
- // }
+	let data = google.visualization.arrayToDataTable(chartdata);
 
-  data = google.visualization.arrayToDataTable(chartdata);
+	let options = {
+		title: '',
+		bar: {groupWidth: '60%'},
+		titleTextStyle: {color: graphColor},
+		legend: 'none',
+		chartArea: {width: '60%', legend:{position: 'none'} },
+		hAxis: {
+			title: 'Stat',
+			viewWindow: {
+				min: 3
+			},
+			gridlines: {
+				count: 16,
+				color: '#ccc'
+			},
+			textStyle: {color: graphColor},
+			titleTextStyle: {color: graphColor}
+		},
+		vAxis: {
+			title: 'Odds of each stat (of 1296)',
+			gridlines: {
+				count: 8,
+				color: '#ccc'
+			},
+			textStyle: {color: graphColor},
+			titleTextStyle: {color: graphColor}
+		},
+		backgroundColor: {fill:'transparent'}
+	};
 
-  options = {
-    title: '',
-    bar: {groupWidth: '60%'},
-    titleTextStyle:{color: graphColor},
-    legend: 'none',
-    chartArea: {width: '60%', legend:{position: 'none'} },
-    hAxis: {
-      title: 'Stat',
-      viewWindow: {
-        min: 3
-      },
-      gridlines: {
-        count: 16,
-        color: '#ccc'
-      },
-      textStyle:{color: graphColor},
-      titleTextStyle:{color: graphColor}
-    },
-    vAxis: {
-      title: 'Odds of each stat (of 1296)',
-      gridlines: {
-        count: 8,
-        color: '#ccc'
-      },
-      textStyle:{color: graphColor},
-      titleTextStyle:{color: graphColor}
-    },
-    backgroundColor: { fill:'transparent' }
-  };
+	//not mobile
+	if(!(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) ) {
+		options.width = 800;
+		options.height = 500;
+	}
 
-  //not mobile
-  if(!(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) ) {
-    options.width = 800;
-    options.height = 500;
-  }
-
-  chart = new google.visualization.ColumnChart(document.getElementById('barchart') );
-  chart.draw(data, options);
+	chart = new google.visualization.ColumnChart(document.getElementById('barchart') );
+	chart.draw(data, options);
 
 
+	//begin chart 2: my rolls
+	let occurences = new Array(16).fill(0);
+	for(let i=0; i<6; i++)
+		occurences[ characters[characters.length-1].stats[i].value -3 ]++;
 
+	chartdata = [['','', { role: 'style' } ]];
+	for(let i=0; i<16; i++)
+		chartdata.push([i+3, occurences[i], '#369']);
+	data = google.visualization.arrayToDataTable(chartdata);
 
-
-    //my rolls barchart
-
-    options.vAxis.title = 'Frequency';
-
-    chartdata = [['','', { role: 'style' } ]];
-
-
-
-   let occurences = new Array(16).fill(0);
-   for(let i=0; i<6; i++)
-      occurences[ characters[characters.length-1].stats[i].value -3 ]++;
-
-    console.log(occurences);
-
-   for(let i=0; i<16; i++) {
-      chartdata.push([i+3, occurences[i], '#369']);
-      // chartdata.push([characters[characters.length-1].stats[i].value, 1, graphColor]);
-   }
-
-    data = google.visualization.arrayToDataTable(chartdata);
-
-
-
-  chart = new google.visualization.ColumnChart(document.getElementById('my-rolls-barchart') );
-  chart.draw(data, options);
-
-
-
+	options.vAxis.title = 'Frequency';
+	chart = new google.visualization.ColumnChart(document.getElementById('my-rolls-barchart') );
+	chart.draw(data, options);
 
 }
